e69a83050fab5fb371c92615847e87cb322eb124
Brian2197 <32573732+Brian2197@users.noreply.github.com>
2017-11-16 17:27:03 -0500

    updaTE

---
+++ Stub.java
@@ -0,0 +1,21 @@
+
+public class Stub extends DataCollectionClass {
+	
+	public String getValue() {
+		String x = null;
+		return x;
+	}
+	public String ReturnTemp() {
+		
+		Temp = "25"; 
+		return "25";
+	}
+	
+	
+	public float getTempVal() {
+		return temp = (float) 30;
+	}
+
+
+
+}
