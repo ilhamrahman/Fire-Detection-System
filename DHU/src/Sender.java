8f1281dc8a9c7aa3d44114cf6aafa7fad4b6366d
Shaviyo Marasinghe <shaviyobm@outlook.com>
2017-11-24 11:29:53 -0500

    Updated Sender Class for DHU

--- DHU/src/Sender.java
+++ DHU/src/Sender.java
@@ -40,118 +40,16 @@ public String tempMessage(boolean isTempHigh) {
 			return noSmoke;
 		}
 	}
-	public JSONObject send(float Temp, String tempMessage, String fireMessage, String smokeMessage) throws JSONException 
-	   {
-	   		  JSONObject params = new JSONObject();
-		         
-		         while (true)
-		         {
-		        	 
-		        	     System.out.println("Sending the analyzed data from the Data Handling Unit to the Android Application... ");
+	public JSONObject send(float Temp, String tempMessage, String fireMessage, String smokeMessage) throws JSONException {
+		
+	    JSONObject params = new JSONObject();
+	    System.out.println("Sending the analyzed data from the Data Handling Unit to the Android Application... ");
 
-		        	     params.put("Current Temperature Value: ", Temp);
-		        		 params.put("Temperature Status: ", tempMessage);
-		        		 params.put("Fire Status: ", fireMessage);
-		        		 params.put("Smoke Status: ", smokeMessage);
-		        		 
-		        		 
-		        		 return params;  
-		        		 
-		        		 /*byte [] data = params.toString().getBytes();
-		        		 DatagramPacket packet = new DatagramPacket(data , data.length, host, port ) ;
-		        		 TimeUnit.SECONDS.sleep(3);
-		        		 socket.send( packet ) ;*/
-		        		 
-		        		// System.out.println("Press ENTER to exit");
-		        		 //message = in.nextLine();
-		        		 //if (message.length()==0) break;
-		         } 
-		         
-		         //System.out.println ("Closing down");
-		      //}
-		      /*catch( Exception e )
-		      {
-		         System.out.println( e ) ;
-		      }
-		      finally
-		      {
-		         if( socket != null )
-		            socket.close() ;
-	      }*/
-		        
-	   }
-	/*
-	public static void main(String[] args) 
-	   {	 // boolean firestatus = false;
-	   		  //Integer temperature;
-	   		  //Integer pressure;
-	   		  JSONObject params = new JSONObject();
-	   		  
-	   
-		      // Check the arguments
-		      if( args.length != 2 )
-		      {
-		         System.out.println( "usage: java UDPSender host port" ) ;
-		         return ;
-		      }
-		      DatagramSocket socket = null ;
-		      try
-		      {
-		         // Convert the arguments first, to ensure that they are valid
-		         InetAddress host = InetAddress.getByName( args[0] ) ;
-		         int port         = Integer.parseInt( args[1] ) ;
-		         socket = new DatagramSocket() ;
-	     
-		         //Scanner in;
-		         //in = new Scanner (System.in);
-		         //String message = null;
-		         while (true)
-		         {
-		        	     //These are the inputs The sender will idealy get from the sensors. 
-		        	     //
-		        		 //System.out.println("Press ENTER if a fire is present. Else type a string");
-		        		 //message = this.tempMessage();
-		        		//if (message.length()==0) firestatus = true;
-		        		 
-		        		// System.out.println("Enter the Current Temperature");
-		        		 //message = in.nextLine();
-		        		// temperature = Integer.parseInt(message);
-		        		 
-		        		// System.out.println("Enter the Current Pressure");
-		        		// message = in.nextLine();
-		        		// pressure = Integer.parseInt(message);
-		        		 
-		        	 
-		        	     System.out.println("Sending the analyzed data from the Data Handling Unit to the Android Application... ");
-		        	     
-		        	     DataAnalysis da = new DataAnalysis();
-		        	     float tempV = da.getTemp();
-
-		        	     params.put("Current Temperature Value: ", tempV);
-		        		 params.put("Temperature Status: ", tempMessage());
-		        		 params.put("Fire Status: ", fireMessage());
-		        		 params.put("Smoke Status: ", smokeMessage());
-		        		 
-		        		 byte [] data = params.toString().getBytes();
-		        		 DatagramPacket packet = new DatagramPacket(data , data.length, host, port ) ;
-		        		 socket.send( packet ) ;
-		        		 
-		        		// System.out.println("Press ENTER to exit");
-		        		 //message = in.nextLine();
-		        		 //if (message.length()==0) break;
-		         } 
-		         //System.out.println ("Closing down");
-		      }
-		      catch( Exception e )
-		      {
-		         System.out.println( e ) ;
-		      }
-		      finally
-		      {
-		         if( socket != null )
-		            socket.close() ;
-	      }
-	   }
-	   */
+	    params.put("Current Temperature Value: ", Temp);
+	    params.put("Temperature Status: ", tempMessage);
+	    params.put("Fire Status: ", fireMessage);
+	    params.put("Smoke Status: ", smokeMessage); 
+	    return params;  
+	}
 	
 }
