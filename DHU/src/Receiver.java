2d3eb9304cf1e1aecbf62fa0ee3019887cf2365f
ilhamrahman <30919695+ilhamrahman@users.noreply.github.com>
2018-05-03 15:44:58 -0400

    Update Receiver.java

--- DHU/src/Receiver.java
+++ DHU/src/Receiver.java
@@ -41,10 +41,7 @@ public class Receiver {
 	             db.insert(data.getTemp(), data.isFireDetected(), data.isSmokeDetected());
 	             
 	             Sender sender = new Sender();     //initialize sender
-	             //float temp = data.getTemp();  //current temperature value
-	             //String tempMessage = sender.tempMessage(data.isTempHigh());   //message for temperature status
-	             //String fireMessage = sender.fireMessage(data.isFireDetected());   //message for fire status
-	             //String smokeMessage = sender.smokeMessage(data.isSmokeDetected());   //message for smoke status
+
 	  
 	          
 	             sender.send(db);     //send the data to the android application
@@ -63,57 +60,7 @@ public class Receiver {
 }
 
 
-/*
-public class Receiver {
-	
-	static InetAddress host;
 
-    public static void main(String[] args) throws UnknownHostException {
-        int port = args.length == 0 ? 57 : Integer.parseInt(args[0]);
-    	//if( args.length != 2 )
-	      //{
-	        // System.out.println( "usage: java UDPSender host port" ) ;
-	         //return ;
-	      //}
-    	//int port = Integer.parseInt( args[0] ) ;
-    	host = InetAddress.getByName( args[1] ) ;
-        new Receiver().run(port);
-        new Sender().send(port);
-    }
-
-    public void run(int port) {    
-      try {
-        DatagramSocket serverSocket = new DatagramSocket(port);
-        byte[] receiveData = new byte[8];
-
-        System.out.printf("Listening on udp:%s:%d%n",
-                InetAddress.getLocalHost().getHostAddress(), port);     
-        DatagramPacket receivePacket = new DatagramPacket(receiveData,
-                           receiveData.length);
-
-        while(true)
-        {
-              serverSocket.receive(receivePacket);
-              String sentence = new String( receivePacket.getData(), 0,
-                                 receivePacket.getLength() );
-              System.out.println("RECEIVED: " + sentence);
-              // now send acknowledgement packet back to sender     
-              InetAddress IPAddress = receivePacket.getAddress();
-              String sendString = "polo";
-              byte[] sendData = sendString.getBytes("UTF-8");
-              DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length,
-                   IPAddress, receivePacket.getPort());
-              serverSocket.send(sendPacket);
-        }
-      } catch (IOException e) {
-              System.out.println(e);
-      }
-      // should close serverSocket in finally block
-    }
-}
- */
-
-//Another possible code for UDPReceiver
 
 
 
